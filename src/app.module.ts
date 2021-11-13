fc3ba978d2855ab1cea7881998e2e63a9a041b96
Maurizio Piu <maurizio.piu@inf.unibe.ch>
2021-11-13 13:28:58 +0100

    updated backends

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PhotosController } from './photos/photos.controller';
-import { PhotosModule } from './photos/photos.module';
+import { Photob64stringModule } from './photob64string/photob64string.module';
 
 @Module({
-  imports: [PhotosModule],
-  controllers: [AppController, PhotosController],
+  imports: [Photob64stringModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
